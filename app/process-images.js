e4293638794a0ab2b459196ce90cc153d2c0afae
Josh Gillies <github@joshgilli.es>
2015-01-02 20:06:06 +1100

    Move image processing into response closure

--- app/process-images.js
+++ app/process-images.js
@@ -7,34 +7,32 @@ var Peeq = require('peeq');
 var AWS = !config.aws || require('./aws');
 var db = require('./db');
 
-var next;
-var images = imageVariations();
-
-images.on('small', function(image) {
-  var dataURI = 'data:image/jpeg;base64,' + image.body.toString('base64');
-  db.images.put(image.name + '!dataUri', dataURI, console.log);
-});
-
-images.on('image', function(image) {
-  var s3obj = new AWS.S3({params: {Bucket: config.aws.s3.bucket, Key: image.name + '.jpg'}});
-  s3obj.upload({ACL: 'public-read', Body: image.body})
-    .send(function(err, data) {
-      if (err) return console.log(err);
-      db.images.put(image.name, data.Location, next);
-    });
-});
-
 var resource = Peeq(config.remoteResource, 60000);
 
 resource.on('response', function processImageResponse(res) {
   var lastModified = new Date(res.headers['last-modified']);
   var timeStamp = lastModified.toISOString();
 
-  next = after(2, db.images.updateIndex(timeStamp));
+  var next = after(2, db.images.updateIndex(timeStamp));
+  var images = imageVariations();
 
   res
     .pipe(new JPEGDecoder)
     .pipe(createImages(images.toObject(), timeStamp));
+
+  images.on('small', function(image) {
+    var dataURI = 'data:image/jpeg;base64,' + image.body.toString('base64');
+    db.images.put(image.name + '!dataUri', dataURI, console.log);
+  });
+
+  images.on('image', function(image) {
+    var s3obj = new AWS.S3({params: {Bucket: config.aws.s3.bucket, Key: image.name + '.jpg'}});
+    s3obj.upload({ACL: 'public-read', Body: image.body})
+      .send(function(err, data) {
+        if (err) return console.log(err);
+        db.images.put(image.name, data.Location, next);
+      });
+  });
 });
 
 resource.on('error', function(err) {
