e7ba12caa2824a44577d5d38bf91f44b75dca494
Josh Gillies <github@joshgilli.es>
2015-01-05 05:59:30 +1100

    Update config. Now with defaults and honors process.env.PORT where available

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ processImages.on('error', function(err) {
   console.log(err);
 });
 
-server.listen(process.env.PORT || config.port, config.host, function() {
+server.listen(config.port, config.host, function() {
   var info = server.address();
   console.log('Server running at "%s:%d"', info.address, info.port);
 });
