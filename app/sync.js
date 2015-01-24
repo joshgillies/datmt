8f8d9abe6501b1cc846d2e895933a1a9ce8654db
Josh Gillies <github@joshgilli.es>
2015-01-24 23:33:39 +1100

    Update sync to fetch from already running application instance

--- app/sync.js
+++ app/sync.js
@@ -1,53 +1,85 @@
 var config = require('config');
 var db = require('./db');
-var s3 = require('dal/s3');
 
-var bucket = config.aws.s3.bucket;
+if (config.syncUrl) {
+  var hyperquest = require('hyperquest');
+  var concat = require('concat-stream');
 
-var getObjects = function getObjects(region) {
-  return function processObjects(err, data) {
-    if (err)
-      return console.log(err);
+  var getJson = function(uri, callback) {
+    var req = hyperquest(uri);
+    req.setHeader('accept', 'application/json');
+    return req;
+  };
 
-    if (data.Contents.length) {
-      s3.getObjects.apply(null, [
-        bucket,
-        region,
-        data.Contents[data.Contents.length - 1].Key,
-        getObjects(region)
-      ]);
-    }
+  getJson(config.syncUrl)
+    .pipe(concat(function(data) {
+      data = JSON.parse(data.toString());
+      Object.keys(data).forEach(function(key) {
+        getJson(config.serviceUrl + '/' + key)
+          .pipe(concat(function(data) {
+            var ops = [
+              { types: 'put', key: data.index + '--large', value: data.imageURI },
+              { types: 'put', key: data.index + '--small', value: data.imageURISmall },
+              { types: 'put', key: data.index + '--small!dataUri', value: data.dataURI }
+            ];
+            db.images.batch(ops, function(err) {
+              if (err) return console.log(err);
+              db.index.put('' + (new Date(data.index)).valueOf(), data.index);
+            });
+          }));
+      });
+    }));
 
-    data.Contents.forEach(function createData(item) {
-      var key = item.Key;
-      if (/small/g.test(key)) {
-        s3.getObjectDetails(bucket, key, function writeDataUri(err, data) {
-          if (err)
-            return console.log(err);
+} else {
+  var s3 = require('dal/s3');
 
-          db.images.put(key.replace('.jpg', '!dataUri'), 'data:image/jpeg;base64,' + data.Body.toString('base64'));
-        });
+  var bucket = config.aws.s3.bucket;
+
+  var getObjects = function getObjects(region) {
+    return function processObjects(err, data) {
+      if (err)
+        return console.log(err);
+
+      if (data.Contents.length) {
+        s3.getObjects.apply(null, [
+          bucket,
+          region,
+          data.Contents[data.Contents.length - 1].Key,
+          getObjects(region)
+        ]);
       }
 
-      db.images.put(
-        key.replace('.jpg', ''),
-        'https://' + bucket + '.s3-' + region + '.amazonaws.com/' + key
-      );
-    });
+      data.Contents.forEach(function createData(item) {
+        var key = item.Key;
+        if (/small/g.test(key)) {
+          s3.getObjectDetails(bucket, key, function writeDataUri(err, data) {
+            if (err)
+              return console.log(err);
 
-    data.Contents.map(function generateKey(item) {
-      return item.Key.replace(/--(small|large).jpg/g,'');
-    }).filter(function unique(value, index, array) {
-      return array.indexOf(value) === index;
-    }).forEach(function writeIndex(key) {
-      db.index.put('' + (new Date(key)).valueOf(), key);
-    });
+            db.images.put(key.replace('.jpg', '!dataUri'), 'data:image/jpeg;base64,' + data.Body.toString('base64'));
+          });
+        }
+
+        db.images.put(
+          key.replace('.jpg', ''),
+          'https://' + bucket + '.s3-' + region + '.amazonaws.com/' + key
+        );
+      });
+
+      data.Contents.map(function generateKey(item) {
+        return item.Key.replace(/--(small|large).jpg/g,'');
+      }).filter(function unique(value, index, array) {
+        return array.indexOf(value) === index;
+      }).forEach(function writeIndex(key) {
+        db.index.put('' + (new Date(key)).valueOf(), key);
+      });
+    };
   };
-};
 
-s3.getBucketRegion(bucket, function getRegion(err, data) {
-  if (err)
-    return console.log(err);
-  var region = data.LocationConstraint;
-  s3.getObjects(bucket, region, getObjects(region));
-});
+  s3.getBucketRegion(bucket, function getRegion(err, data) {
+    if (err)
+      return console.log(err);
+    var region = data.LocationConstraint;
+    s3.getObjects(bucket, region, getObjects(region));
+  });
+}
